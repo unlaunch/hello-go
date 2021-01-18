daac591fa92dfa52c5263d8f2ecc398567a87fb4
Umer Mansoor <umermk3@gmail.com>
2021-01-18 00:00:40 -0800

    UL-809 Update client methods because SDK changed

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/unlaunch/hello-go
 
 go 1.15
 
-require github.com/unlaunch/go-sdk v0.0.1
+require github.com/unlaunch/go-sdk v0.0.3
--- hello.go
+++ hello.go
@@ -8,6 +8,13 @@ import (
 )
 
 func main() {
+	helloGoSimple()
+	helloGoAdvanced()
+	helloGoAttributes()
+}
+
+func helloGoSimple() {
+	fmt.Println("-- Running Hello Go Simple --")
 	const apiKey = "prod-server-7f05812f-cd5a-41dd-b79b-6f30d84cd335"
 	const flagKey = "mfa-feature"
 
@@ -25,21 +32,130 @@ func main() {
 
 	unlaunchClient := factory.Client()
 
-	if err = unlaunchClient.BlockUntilReady(5 * time.Second); err != nil {
+	if err = unlaunchClient.AwaitUntilReady(5 * time.Second); err != nil {
 		fmt.Printf("Unlaunch Client isn't ready %s\n", err)
 	}
 
 	variation := unlaunchClient.Variation(flagKey, "user123", nil)
 	fmt.Printf("The variation for feature is: %s\n", variation)
 
+	unlaunchClient.Shutdown()
+
+	fmt.Printf("Client shutdown successfully? %v\n\n", unlaunchClient.IsShutdown())
+}
+
+func helloGoAdvanced() {
+	fmt.Println("-- Running Hello Go Advanced which passes attributes and prints evaluation reason --")
+	const apiKey = "prod-server-7f05812f-cd5a-41dd-b79b-6f30d84cd335"
+	const flagKey = "mfa-feature"
+
+	cfg := client.DefaultConfig()
+	cfg.LoggerConfig = &logger.LogOptions{
+		Level:    "INFO",
+		Colorful: true,
+	}
+
+	factory, _ := client.NewUnlaunchClientFactory(apiKey, cfg)
+
+	unlaunchClient := factory.Client()
+
+	if err := unlaunchClient.AwaitUntilReady(5 * time.Second); err != nil {
+		fmt.Printf("Unlaunch Client isn't ready %s\n", err)
+	}
+
+	// Let's use the Feature(...) function to get both variation and evaluation reason
 	// Let's pass some attributes
 	attr := make(map[string]interface{})
 	attr["registered"] = true
 
-	variation = unlaunchClient.Variation(flagKey, "user123", attr)
-	fmt.Printf("The variation for feature with registered=true is: %s\n", variation)
+	feature := unlaunchClient.Feature(flagKey, "user123", attr)
+	fmt.Printf("The variation for feature is: %s. Evaluation reason is: %s\n",
+		feature.Variation, feature.EvaluationReason)
 
 	unlaunchClient.Shutdown()
 
-	fmt.Printf("Client shutdown successfully? %v", unlaunchClient.IsShutdown())
+	fmt.Printf("Client shutdown successfully? %v\n\n", unlaunchClient.IsShutdown())
+}
+
+func helloGoAttributes() {
+	fmt.Println("-- Running Hello Go Attributes which passes attributes and prints evaluation reason --")
+	const apiKey = "prod-server-7f05812f-cd5a-41dd-b79b-6f30d84cd335"
+	const flagKey = "mfa-feature"
+
+	cfg := client.DefaultConfig()
+	cfg.LoggerConfig = &logger.LogOptions{
+		Level:    "INFO",
+		Colorful: true,
+	}
+
+	factory, _ := client.NewUnlaunchClientFactory(apiKey, cfg)
+
+	unlaunchClient := factory.Client()
+
+	if err := unlaunchClient.AwaitUntilReady(5 * time.Second); err != nil {
+		fmt.Printf("Unlaunch Client isn't ready %s\n", err)
+	}
+
+	// Let's pass some attributes
+	attr := make(map[string]interface{}) // map to store all attributes
+
+	attr["registered"] = true
+	attr["device"] = "iphone"
+	attr["age"] = 30
+	attr["startDate"] = time.Now().UTC().Unix()
+
+	// let's make a "Set" to store some user Id
+	// We use map because "Go" doesn't have a set type. Only keys are used. The value could be anything and is ignored
+	userIDs := make(map[string]bool)
+	userIDs["user123@gmail.com"] = true
+	userIDs["userabc@gmail.com"] = true
+
+	feature := unlaunchClient.Feature(flagKey, "user123", attr)
+	fmt.Printf("The variation for feature is: %s\n", feature.Variation)
+
+	unlaunchClient.Shutdown()
+
+	fmt.Printf("Client shutdown successfully? %v\n\n", unlaunchClient.IsShutdown())
+}
+
+func helloGoConfiguration() {
+	fmt.Println("-- Running Hello Go Attributes which passes attributes and prints evaluation reason --")
+	const apiKey = "prod-server-7f05812f-cd5a-41dd-b79b-6f30d84cd335"
+	const flagKey = "mfa-feature"
+
+	cfg := client.DefaultConfig()
+	// Customize default client
+	cfg.PollingInterval = 15 * time.Second // How often flags are fetched
+	cfg.HTTPTimeout = 3 * time.Second
+	cfg.MetricsFlushInterval = 30 * time.Second // How often metrics are sent
+	cfg.MetricsQueueSize = 500
+
+	factory, _ := client.NewUnlaunchClientFactory(apiKey, cfg)
+
+	unlaunchClient := factory.Client()
+
+	if err := unlaunchClient.AwaitUntilReady(5 * time.Second); err != nil {
+		fmt.Printf("Unlaunch Client isn't ready %s\n", err)
+	}
+
+	// Let's pass some attributes
+	attr := make(map[string]interface{}) // map to store all attributes
+
+	attr["registered"] = true
+	attr["device"] = "iphone"
+	attr["age"] = 30
+	attr["startDate"] = time.Now().UTC().Unix()
+
+	// let's make a "Set" to store some user Id
+	// We use map because "Go" doesn't have a set type. Only keys are used. The value could be anything and is ignored
+	userIDs := make(map[string]bool)
+	userIDs["user123@gmail.com"] = true
+	userIDs["userabc@gmail.com"] = true
+
+	feature := unlaunchClient.Feature(flagKey, "user123", attr)
+	fmt.Printf("The variation for feature is: %s\n", feature.Variation)
+
+	unlaunchClient.Shutdown()
+
+	fmt.Printf("Client shutdown successfully? %v\n\n", unlaunchClient.IsShutdown())
 }
